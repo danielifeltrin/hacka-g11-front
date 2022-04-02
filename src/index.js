6bf6bd872351a0a3adf97d509569544a679cdd82
XimitiCB <ximiticb@gmail.com>
2022-04-02 15:12:28 -0300

    GA

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ReactGA from 'react-ga';
 import {getCLS, getFID, getLCP} from 'web-vitals';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-9NG4NQFX0C');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
